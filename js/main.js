e4d0fd9ae1db7967f2e9225a6be9781bad9c232c
Fabian <fabiing10@gmail.com>
2016-09-30 23:21:59 -0500

    Validaciones y complementar funciones (Fabian) & Upload to Heroku

--- js/main.js
+++ js/main.js
@@ -27,5 +27,36 @@ window.onload = function()
     return result;
   };
 
+  //Fields Validations
+    while(isNaN(number_one) == true){
+      number_one = prompt("Ingrese el primer numero");
+    }
+    console.log("Numero 1: "+number_one)
+    while(isNaN(number_two) == true){
+      number_two = prompt("Ingrese el segundo numero");
+    }
+    console.log("Numero 2: "+number_two)
+    //Get operation value
+    var operation = prompt("Ingrese operacion a realizar");
+    var cond = Number.isNaN(operation);
+    if(cond){
+      var operation  = param.toLowerCase();
+    }
+
+
+    if(operation === "suma" || operation === "mas" || operation === "+"){
+      value = sumar(number_one,number_two);
+    }else if(operation === "resta" || operation ==="menos" || operation ==="-"){
+      value =  restar(number_one,number_two);
+    }else if(operation === "multiplicar" || operation ==="por" || operation ==="*"){
+      value =  multiplicar(number_one,number_two);
+    }else if(operation === "dividir" || operation ==="/"){
+      value =  dividir(number_one,number_two);
+    }else{
+      value = "Error en la operacion";
+    }
+
+
+    document.getElementById("result").innerHTML = value;
 
 }
