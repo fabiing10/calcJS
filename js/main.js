f5b5b62b68bc7db3cb1302cbe85c333299a97af8
Fabian <fabiing10@gmail.com>
2016-09-30 23:19:27 -0500

    Estructura base y funciones Engie

---
+++ js/main.js
@@ -0,0 +1,31 @@
+window.onload = function()
+{
+  //Variables Definition
+  let result;
+  let value;
+  var number_one;
+  var number_two;
+
+
+  var sumar = function(n1, n2){
+     result = parseFloat(n1) + parseFloat(n2);
+    return result;
+  };
+
+  var restar = function(n1, n2){
+    result = parseFloat(n1) - parseFloat(n2);
+    return result;
+  };
+
+  var multiplicar = function(n1, n2){
+    result = parseFloat(n1) * parseFloat(n2);
+    return result;
+  };
+
+  var dividir = function(n1, n2){
+    result = parseFloat(n1) / parseFloat(n2);
+    return result;
+  };
+
+
+}
